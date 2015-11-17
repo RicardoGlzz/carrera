b013c44cabc68377f57792b82bbb08da24704404
Ricardo Gonzalez <ricardogonzalezlopez1@gmail.com>
2015-11-17 11:36:22 -0600

    vistas

---
+++ resources/views/index.blade.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Carrera</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body>
+    <section id="wrapper">
+        <nav>
+            <section>
+                <div class="logo-menu">
+                    <img src="img/logo.png" alt="Logo principal en menu">
+                </div>
+                <div class="menu">
+                    <ul>
+                        <a href="">
+                            <li>REGISTRO</li>
+                        </a>
+                        <a href="">
+                            <li>¿POR QUE 12 K?</li>
+                        </a>
+                        <a href="">
+                            <li>CORREDORES</li>
+                        </a>
+                        <a href="">
+                            <li>TOP 5</li>
+                        </a>
+                        <a href="">
+                            <li>C.A.S.A</li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="redes">
+                    <ul>
+                        <a href="">
+                            <li><i class="fa fa-facebook"></i></li>
+                        </a>
+                        <a href="">
+                            <li><i class="fa fa-twitter"></i></li>
+                        </a>
+                    </ul>
+                </div>
+            </section>
+             <article></article>
+        </nav>
+        <section id="vista-registro">
+            <section class="cont-canvas">
+                <canvas id="canvas" width="900" height="400">
+                    Si estas viendo este mensaje, significa que necesitas utilizar la ultima version de chrome, firefox o opera.
+                </canvas>
+            </section>
+            <section class="botones-canvas">
+             <!--    <div class="logo-registro">
+                    <img src="" alt="logo de registro">
+                </div> -->
+                <div class="km-recorridos">
+                    <figure>
+                        <img src="img/distancia.png" alt="imagen de distancia recorrida">
+                    </figure>
+                    <div id="numero-metros">
+                        0m
+                    </div>
+                </div>
+                <div class="boton-registro">
+                    <a target="_blank" href="{{ URL::to('registro') }}">
+                       <h1>APORTA</h1>
+                       <h4>Haz click aqui</h4>
+                    </a>
+                </div>
+                <div class="seguir-corriendo">
+                    <h1>¡Que siga corriendo!</h1>
+                    <img src="img/mono.png" alt="mono">
+                </div>
+            </section>
+        </section>
+        <section id="descripcion">
+             <article class="inverso"></article>
+            <div class="logo-desc">
+                <img src="img/logo_desc.png" alt="logo de descripcion">
+            </div>
+            <div class="desc">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente doloribus distinctio adipisci repudiandae delectus, illum quos eum eos explicabo dolor voluptate velit nesciunt! Ex quisquam nihil harum in amet eum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde iste ipsa eaque nesciunt harum consequatur numquam aliquid nihil laudantium distinctio nobis eos hic, quibusdam repudiandae quasi impedit dolorem saepe commodi?</p>
+            </div>
+            <br>
+                <button id="ver-desc" class="boton-ver">Ver Más</button>
+            <br>
+             <article class="triangulo"></article>
+        </section>
+        <section id="top5">
+            <section id="titulotop">
+                <div>
+                    <img src="img/titulo_top.png" alt="imagen de participante top">
+                </div>
+            </section>
+            <section id="tops">
+                <section>
+                    <div class="div_top">
+                        <figure class="img_liston">
+                            <img src="img/liston_top.png" alt="imagen de liston top">    
+                        </figure>
+                        <figure class="cont_top">
+                            <img  class="img_top"src="img/pug.jpg" alt="imagen de participante top">
+                        </figure>
+                    </div>
+                    <div class="div_top datos_top">
+                        <h3 class="lugar">Primer lugar</h3>
+                        <h3 class="dist-recorrida">135m</h3>
+                        <h3 class="nombre-top">Pug</h3>
+                    </div>
+                </section>
+
+                  <section>
+                    <div class="div_top">
+                        <figure class="img_liston">
+                            <img src="img/liston_top.png" alt="imagen de liston top">    
+                        </figure>
+                        <figure class="cont_top">
+                            <img  class="img_top"src="img/pug_2.jpg" alt="imagen de participante top">
+                        </figure>
+                    </div>
+                    <div class="div_top datos_top">
+                        <h3 class="lugar">Segundo lugar</h3>
+                        <h3 class="dist-recorrida">105m</h3>
+                        <h3 class="nombre-top">Pug</h3>
+                    </div>
+                </section>
+
+                 <section>
+                    <div class="div_top">
+                        <figure class="img_liston">
+                            <img src="img/liston_top.png" alt="imagen de liston top">    
+                        </figure>
+                        <figure class="cont_top">
+                            <img  class="img_top"src="img/pug_3.jpeg" alt="imagen de participante top">
+                        </figure>
+                    </div>
+                    <div class="div_top datos_top">
+                        <h3 class="lugar">Tercer lugar</h3>
+                        <h3 class="dist-recorrida">100m</h3>
+                        <h3 class="nombre-top">Pug</h3>
+                    </div>
+                </section>
+
+                 <section>
+                    <div class="div_top">
+                        <figure class="img_liston">
+                            <img src="img/liston_top.png" alt="imagen de liston top">    
+                        </figure>
+                        <figure class="cont_top">
+                            <img  class="img_top"src="img/pug.jpg" alt="imagen de participante top">
+                        </figure>
+                    </div>
+                    <div class="div_top datos_top">
+                        <h3 class="lugar">Cuarto lugar</h3>
+                        <h3 class="dist-recorrida">90m</h3>
+                        <h3 class="nombre-top">Pug</h3>
+                    </div>
+                </section>
+
+                  <section>
+                    <div class="div_top">
+                        <figure class="img_liston">
+                            <img src="img/liston_top.png" alt="imagen de liston top">    
+                        </figure>
+                        <figure class="cont_top">
+                            <img  class="img_top"src="img/pug.jpg" alt="imagen de participante top">
+                        </figure>
+                    </div>
+                    <div class="div_top datos_top">
+                        <h3 class="lugar">Quinto lugar</h3>
+                        <h3 class="dist-recorrida">80m</h3>
+                        <h3 class="nombre-top">Pug</h3>
+                    </div>
+                </section>
+            
+            </section>
+        </section>
+        <section id="participantes">
+            <article class="inverso"></article>
+            <div class="titulo-corredores">
+                <figure>
+                    <img src="img/titulo_corredores.png" alt="imagen de liston de corredores">
+                </figure>
+            </div>
+             <section>
+                    <div class="div_top">
+                        <figure class="liston_rojo">
+                            <img src="img/liston_rojo.png" alt="imagen de liston rojo">    
+                        </figure>
+                        <figure class="cont_part">
+                            <img  class="img_top"src="img/pug.jpg" alt="imagen de participante">
+                        </figure>
+                    </div>
+                    <div class="div_top datos_top">
+                        <h3 class="dist-recorrida">50m</h3>
+                        <h3 class="nombre-top">Pug</h3>
+                    </div>
+            </section>
+
+             <section>
+                    <div class="div_top">
+                        <figure class="liston_rojo">
+                            <img src="img/liston_rojo.png" alt="imagen de liston rojo">    
+                        </figure>
+                        <figure class="cont_part">
+                            <img  class="img_top"src="img/pug.jpg" alt="imagen de participante">
+                        </figure>
+                    </div>
+                    <div class="div_top datos_top">
+                        <h3 class="dist-recorrida">50m</h3>
+                        <h3 class="nombre-top">Pug</h3>
+                    </div>
+            </section>
+
+             <section>
+                    <div class="div_top">
+                        <figure class="liston_rojo">
+                            <img src="img/liston_rojo.png" alt="imagen de liston rojo">    
+                        </figure>
+                        <figure class="cont_part">
+                            <img  class="img_top"src="img/pug.jpg" alt="imagen de participante">
+                        </figure>
+                    </div>
+                    <div class="div_top datos_top">
+                        <h3 class="dist-recorrida">50m</h3>
+                        <h3 class="nombre-top">Pug</h3>
+                    </div>
+            </section>
+             <section>
+                    <div class="div_top">
+                        <figure class="liston_rojo">
+                            <img src="img/liston_rojo.png" alt="imagen de liston rojo">    
+                        </figure>
+                        <figure class="cont_part">
+                            <img  class="img_top"src="img/pug.jpg" alt="imagen de participante">
+                        </figure>
+                    </div>
+                    <div class="div_top datos_top">
+                        <h3 class="dist-recorrida">50m</h3>
+                        <h3 class="nombre-top">Pug</h3>
+                    </div>
+            </section>
+             <section>
+                    <div class="div_top">
+                        <figure class="liston_rojo">
+                            <img src="img/liston_rojo.png" alt="imagen de liston rojo">    
+                        </figure>
+                        <figure class="cont_part">
+                            <img  class="img_top"src="img/pug.jpg" alt="imagen de participante">
+                        </figure>
+                    </div>
+                    <div class="div_top datos_top">
+                        <h3 class="dist-recorrida">50m</h3>
+                        <h3 class="nombre-top">Pug</h3>
+                    </div>
+            </section>
+            <br>
+            <button id="ver-part" class="boton-ver">Ver Más</button>
+            <br>
+             <article class="triangulo"></article>
+        </section>
+        <footer>
+            <div></div>
+            <div></div>
+            <div>
+                <img src="" alt="logo de virtua">
+            </div>
+        </footer>
+    </section>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="js/app.js"></script>
+</body>
+
+</html>
---
+++ resources/views/registro.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Registro</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+<section class="registro-wrap">
+	<div class="franja">
+        <figure>
+            <img src="img/logo.png" alt="">
+        </figure>
+    </div>
+    <section class="contenedor">
+    	
+    </section>
+</section>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="js/app.js"></script>	
+</body>
+</html>
