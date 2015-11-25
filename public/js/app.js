fee4f337207539e163b9de5ae8d66d64ee6ad97f
Ricardo Gonzalez <ricardogonzalezlopez1@gmail.com>
2015-11-25 12:57:43 -0600

    canvas y fuente

--- public/js/app.js
+++ public/js/app.js
@@ -3,107 +3,105 @@ $(function()
 
     var total = 12000; //metros totales
     var distancia_linea = 1200; //metros por linea, suponiendo que se dibujaran 10 lineas
-   
+
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
 
     
     window.requestAnimFrame = (function (callback) {
-   
-    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
-        window.setTimeout(callback, 1000 / 60);
-    };
-})();
 
-var pathArray = [];
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+    })();
+
+    var pathArray = [];
 
 // VARIABLE DE DISTANCIA TOTAL
 // var distancia_total = $("#numero-metros").text();
 // distancia_total.replace('m','');
 // var numero = parseInt(distancia_total);
-// console.log(numero);
-// DISTANCIA TOTAL DE PRUEBA
-var numero = 11000;
+var numero = 12000;
 
 
 // Coordenada inicial
 
-        var inicial =
-        {
-            x: 50,
-            y: 230
-        }
+var inicial =
+{
+    x: 50,
+    y: 280
+}
 
 // Coordenadas para distancia de lineas
-    var primera = 
-            {
-                x: 50,
-                y: 50
-            }
+var primera = 
+{
+    x: 50,
+    y: 50
+}
 
-    var segunda = 
-        {
-            x: 260,
-            y: 90
-        }
+var segunda = 
+{
+    x: 260,
+    y: 90
+}
 
-    var tercera = 
-        {
-            x: 490,
-            y: 60
-        }
+var tercera = 
+{
+    x: 490,
+    y: 60
+}
 
 var cuarta = 
-    {
-        x: 710,
-        y: 60
-    }
+{
+    x: 710,
+    y: 60
+}
 
 
 var quinta = 
-    {
-        x: 720,
-        y: 240
-    }
+{
+    x: 720,
+    y: 240
+}
 
 
 var sexta = 
-    {
-        x: 720,
-        y: 420
-    }
+{
+    x: 720,
+    y: 420
+}
 
 var septima = 
-    {
-        x: 500,
-        y: 440
-    }
+{
+    x: 500,
+    y: 440
+}
 
 var octava = 
-    {
-        x: 360,
-        y: 300
-    }
+{
+    x: 320,
+    y: 300
+}
 
 var novena = 
-    {
-        x: 60,
-        y: 360
-    }
+{
+    x: 60,
+    y: 360
+}
 
 
 var decima = 
-    {
-        x: 70,
-        y: 230
-    }
+{
+    x: 70,
+    y: 230
+}
 
-    
+
 
     // PRIMERA LINEA
     if( numero <= 1200)
     {
-            
+
             // Calcular distancia entre dos puntos
             x1 = 50;
             y1 = 230;
@@ -115,7 +113,7 @@ var decima =
             var val = inicial.y-cy;
             var valor = Math.round(val);
             console.log(valor);
-            // console.log(distancia);
+          
             // console.log(valor);
             
             pathArray.push(inicial);  
@@ -124,14 +122,14 @@ var decima =
                 x: 50,
                 y: valor
             }
-        );
-           
-    }
+            );
+
+        }
 
     // SEGUNDA LINEA
     else if(numero <= 2400)
     {       
-            numero-=1200;
+        numero-=1200;
             // Calcular distancia entre dos puntos
             x1 = 50;
             y1 = 50;
@@ -153,13 +151,13 @@ var decima =
                 x: valor,
                 y: 80
             }
-        );  
-    }
+            );  
+        }
 
     // TERCERA LINEA
     else if(numero <= 3600)
     {   
-            numero-=2400;
+        numero-=2400;
             // Calcular distancia entre dos puntos
             x1 = 260;
             y1 = 90;
@@ -183,13 +181,13 @@ var decima =
                 x: valor,
                 y: 60
             }
-        );  
-    }
+            );  
+        }
 
     // CUARTA LINEA
     else if(numero <= 4800)
     {
-            numero-=3600;
+        numero-=3600;
             // Calcular distancia entre dos puntos
             x1 = 490;
             y1 = 60;
@@ -213,13 +211,14 @@ var decima =
                 x: valor,
                 y: 60
             }
-        );  
-    }
+            );  
+        }
 
      // QUINTA LINEA
-    else if(numero <= 6000)
-    {
-            numero-=4800;
+     else if(numero <= 6000)
+     {
+        
+        numero-=4800;
             // Calcular distancia entre dos puntos
             x1 = 710;
             y1 = 60;
@@ -230,7 +229,7 @@ var decima =
             var cy = (numero * distancia) / 1200;
             var val = cuarta.y+cy;
             var valor = Math.round(val);
-            // console.log(val);
+            // console.log(valor);
             // console.log(cx);
             // console.log(distancia);
 
@@ -244,13 +243,18 @@ var decima =
                 x: 720,
                 y: valor
             }
-        );  
-    }
+            );  
+            console.log(pathArray.length);
+           
+
+
+
+        }
 
     // SEXTA LINEA
     else if(numero <= 7200)
     {
-            numero-=6000;
+        numero-=6000;
             // Calcular distancia entre dos puntos
             x1 = 720;
             y1 = 240;
@@ -261,7 +265,7 @@ var decima =
             var cy = (numero * distancia) / 1200;
             var val = quinta.y+cy;
             var valor = Math.round(val);
-            // console.log(val);
+           
             // console.log(cx);
             // console.log(distancia);
 
@@ -276,13 +280,13 @@ var decima =
                 x: 720,
                 y: valor
             }
-        );  
-    }
+            );  
+        }
 
     // SEPTIMA LINEA
     else if(numero <= 8400)
     {
-            numero-=7200;
+        numero-=7200;
             // Calcular distancia entre dos puntos
             x1 = 720;
             y1 = 440;
@@ -309,17 +313,17 @@ var decima =
                 x: valor,
                 y: 400
             }
-        );  
-    }
+            );  
+        }
 
     // OCTAVA LINEA
     else if(numero <= 9600)
     {
-            numero-=8400;
+        numero-=8400;
             // Calcular distancia entre dos puntos
             x1 = 500;
             y1 = 440;
-            x2 = 360;
+            x2 = 320;
             y2 = 300;
             var distancia = Math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) );
             // obtener los puntos en x o en y
@@ -343,13 +347,13 @@ var decima =
                 x: valor,
                 y: 300
             }
-        );  
-    }
+            );  
+        }
 
     // NOVENA LINEA
     else if(numero <= 10800)
     {
-            numero-=9600;
+        numero-=9600;
             // Calcular distancia entre dos puntos
             x1 = 360;
             y1 = 260;
@@ -378,8 +382,8 @@ var decima =
                 x: valor,
                 y: 360
             }
-        );  
-    }
+            );  
+        }
 
     // DECIMA LINEA
     else if(numero <= 12000)
@@ -389,7 +393,7 @@ var decima =
             x1 = 60;
             y1 = 360;
             x2 = 50;
-            y2 = 230;
+            y2 = 280;
             var distancia = Math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) );
             // obtener los puntos en x o en y
             var cy = (numero * distancia) / 1200;
@@ -414,116 +418,147 @@ var decima =
                 x: 60,
                 y: valor
             }
-        );  
-    }
-   
-var polypoints = makePolyPoints(pathArray);
+            );  
+        }
 
-var position = 0;
-var speed = 1;
-animate();
+        var puntosclave = hacerPuntosClave(pathArray);
+        var posicion = 0;
+        var velocidad = 5;
+        animate();
 
-var fps = 40;
+        var fps = 40;
+
+
 
 function animate() {
 
     // lineas guia
-    ctx.lineWidth = 5;
+    // ctx.lineWidth = 5;
 
-    ctx.beginPath();
-    ctx.moveTo(150, 200);
-    ctx.lineTo(130, 10);
-    ctx.strokeStyle = 'red';
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(150, 200);
+    // ctx.lineTo(130, 10);
+    // ctx.strokeStyle = 'red';
+    // ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(130, 10);
-    ctx.lineTo(340, 80);
-    ctx.strokeStyle = 'green';
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(130, 10);
+    // ctx.lineTo(340, 80);
+    // ctx.strokeStyle = 'green';
+    // ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(340, 80);
-    ctx.lineTo(560, 30);
-    ctx.strokeStyle = 'gold';
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(340, 80);
+    // ctx.lineTo(560, 30);
+    // ctx.strokeStyle = 'gold';
+    // ctx.stroke();
     
 
-    ctx.beginPath();
-    ctx.moveTo(560, 30);
-    ctx.lineTo(800, 40);
-    ctx.strokeStyle = 'blue';
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(560, 30);
+    // ctx.lineTo(800, 40);
+    // ctx.strokeStyle = 'blue';
+    // ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(800, 40);
-    ctx.lineTo(800, 220);
-    ctx.strokeStyle = 'gold';
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(800, 40);
+    // ctx.lineTo(800, 220);
+    // ctx.strokeStyle = 'gold';
+    // ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(800, 220);
-    ctx.lineTo(800, 380);
-    ctx.strokeStyle = 'red';
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(800, 220);
+    // ctx.lineTo(800, 380);
+    // ctx.strokeStyle = 'red';
+    // ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(800, 380);
-    ctx.lineTo(550, 380);
-    ctx.strokeStyle = 'blue';
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(800, 380);
+    // ctx.lineTo(550, 380);
+    // ctx.strokeStyle = 'blue';
+    // ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(550, 380);
-    ctx.lineTo(350, 250);
-    ctx.strokeStyle = 'gold';
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(550, 380);
+    // ctx.lineTo(350, 250);
+    // ctx.strokeStyle = 'gold';
+    // ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(350, 250);
-    ctx.lineTo(125, 360);
-    ctx.strokeStyle = 'red';
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(350, 250);
+    // ctx.lineTo(125, 360);
+    // ctx.strokeStyle = 'red';
+    // ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(125, 360);
-    ctx.lineTo(150, 200);
-    ctx.strokeStyle = 'blue';
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(125, 360);
+    // ctx.lineTo(150, 200);
+    // ctx.strokeStyle = 'blue';
+    // ctx.stroke();
 
     setTimeout(function () {
         requestAnimFrame(animate);
 
         // calcular nueva posicion
-        position += speed;
-        if (position > polypoints.length - 1) {
+        posicion += velocidad;
+        if (posicion > puntosclave.length - 1) {
             return;
         }
-        var pt = polypoints[position];
+        var pt = puntosclave[posicion];
+        console.log(posicion);
 
 
         // dibujar
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.save();
-        ctx.beginPath();
-        ctx.translate(pt.x, pt.y);
-        // Objeto de la imagen del corredor
-        img = new Image();
-        // url de imagen
-        
-        img.src = "img/mono_animar.png";
-        ctx.drawImage(img,0,-80);
-        ctx.restore();
+         if(posicion >= 402)
+            {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.save();
+                ctx.beginPath();
+                ctx.translate(pt.x, pt.y);
+                // Objeto de la imagen del corredor
+                 img = new Image();
+                // url de imagen
+                img.src = "img/mono_animar_2.png";
+                ctx.drawImage(img,0,-80);
+                ctx.restore();
+            }
+          if(posicion >= 900)
+            {
+                console.log("ya llego");
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.save();
+                ctx.beginPath();
+                ctx.translate(pt.x, pt.y);
+                // Objeto de la imagen del corredor
+                 img = new Image();
+                // url de imagen
+                img.src = "img/termino.png";
+                ctx.drawImage(img,0,-80);
+                ctx.restore();
+            }  
+            else 
+            {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.save();
+                ctx.beginPath();
+                ctx.translate(pt.x, pt.y);
+                // Objeto de la imagen del corredor
+                 img = new Image();
+                // url de imagen
+                img.src = "img/mono_animar.png";
+                ctx.drawImage(img,0,-80);
+                ctx.restore();   
+            }
 
+       
+        
     }, 1000 / fps);
+
 }
 
-function makePolyPoints(pathArray) {
+function hacerPuntosClave(pathArray) {
 
-    var points = [];
-
-
-
+    var puntos = [];
 
     for (var i = 1; i < pathArray.length; i++) 
     {
@@ -535,15 +570,14 @@ function makePolyPoints(pathArray) {
         {
             var x = startPt.x + dx * n / 100;
             var y = startPt.y + dy * n / 100;
-            points.push({
+            puntos.push({
                 x: x,
                 y: y
             });
         }
-         console.log(points);
     }
-    return (points);
-   
+    return (puntos);
+
 }
 
 });
